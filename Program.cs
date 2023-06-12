d3c03161649ba4c76ac6fe95f6f67fef9054627c
sipra <sipramishra085@gmail.com>
2023-06-13 00:44:46 +0530

    Added Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MoodAnalyserDay20
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program !!!");
+        }
+    }
+}
