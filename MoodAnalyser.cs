7b49d641e0cf9dc21c7e12a84576217588e199d6
sipra <sipramishra085@gmail.com>
2023-06-13 00:52:17 +0530

    Added  file

---
+++ MoodAnalyser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDay20
+{
+    public class MoodAnalyser
+    {
+        private string message;
+
+        public MoodAnalyser() //DEAFULT
+        {
+
+        }
+        public MoodAnalyser(string message) //PARAMERTEIZED
+        {
+            this.message = message;
+        }
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "Sad Mood";
+            }
+            else
+            {
+                return "Happy Mood";
+            }
+
+        }
+    }
+}
